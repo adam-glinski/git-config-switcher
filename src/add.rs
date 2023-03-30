2d794865f5f7d37c39753901683b0ea4027b5e17
adam-glinski <adam.s.glinski@gmail.com>
2023-03-31 00:37:27 +0200

    dont mind me
    have to work on laptop...

--- src/add.rs
+++ src/add.rs
@@ -1,7 +1,16 @@
 use clap::ArgMatches;
 
 // TODO: Rename param
-pub fn on_add(_sub_matches: &ArgMatches) {
-    println!("on_add called");
-}
+pub fn on_add(sub_matches: &ArgMatches) {
+//    if let Some(ref m) = sub_matches.subcommand_matches("add"){
+//        if let Some(ref name) = m.get_one::<String>("name") {
+//            println!("{}", name.as_str());
+//        }
+//    };
 
+    let Some(ref m) = sub_matches.subcommand_matches("add") else { return };
+    let Some(ref name) = m.get_one::<String>("name") else { panic!("Missing name! Use help command to view usage.") };
+    let Some(ref mail) = m.get_one::<String>("email") else { panic!("Missing email! Use help command to view usage.") };
+
+    println!("Hello, {} your email is {}", name, mail);
+}
--- src/cli.rs
+++ src/cli.rs
@@ -1,18 +1,19 @@
 // mod types;
 
-use clap::{arg, command, ArgMatches, Command};
+use clap::{arg, ArgMatches, Command, Arg};
 use std::collections::HashMap;
 
 
 /// @TODO: Change name to smthign more accurate
 pub fn show_cli() -> ArgMatches {
-    let matches = command!()
-        .propagate_version(true)
-        .subcommand_required(true)
+    let matches = Command::new("gitswitcher")
+        .version("0.1.0")
+        .about("Git config manager.")
         .arg_required_else_help(true)
         .subcommand(
             Command::new("add")
                 .about("Adds new author to the list.")
+                .arg(Arg::new("name"))
                 .args([
                     arg!([NAME] "Author name."),
                     arg!([EMAIL] "Author email."),
@@ -32,13 +33,22 @@ pub fn show_cli() -> ArgMatches {
         )
         .get_matches();
 
+
     return matches;
+    /*
+    if let Some(ref matches) = matches.subcommand_matches("add") {
+        if let Some(name) = matches.get_one::<String>("name") {
+            println!("name {}", name.as_str());
+        };
+    }
+     */
 }
 
 pub fn resolve_params(matches: &ArgMatches, function_map: HashMap<String, fn(&ArgMatches)>) {
     let on_add = function_map.get("add").unwrap();
     match matches.subcommand() {
-        Some(("add", sub_matches)) => on_add(sub_matches),
+        // @TODO: rearrange
+        Some(("add", _sub_matches)) => on_add(matches),
 
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
