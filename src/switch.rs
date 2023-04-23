8e61961a7694826cc644f5589b5b399e34fed87c
adam-glinski <adam.s.glinski@gmail.com>
2023-04-23 23:36:12 +0200

    ensured that the alias exists in conifg_map

--- src/switch.rs
+++ src/switch.rs
@@ -1,14 +1,33 @@
-use clap::ArgMatches;
-use std::process::Command;
 use crate::types::ConfigsMap;
+use clap::ArgMatches;
+use std::io::{stdout, Write};
+use std::process::Command;
 
 pub fn on_switch(matches: &ArgMatches, configs_map: &mut ConfigsMap) {
     let Some(ref alias) = matches.get_one::<String>("alias")
         else { panic!("Failed to read alias from ArgMatches") };
 
-    // TODO: Make sure the alias exists in the map
-    let config = configs_map.get(&alias.to_string()).unwrap();
-    let git_name_output = Command::new("git").args(["config", "--global", "user.name", &config.name]).output().unwrap();
-    let git_email_output = Command::new("git").args(["config", "--global", "user.email", &config.email]).output().unwrap();
-    // println!("{}, {}", git_name_output.stdout, git_email_output.stdout);
+    match configs_map.get(&alias.to_string()) {
+        Some(config) => {
+            let git_name_output = Command::new("git")
+                .args(["config", "--global", "user.name", &config.name])
+                .output()
+                .unwrap();
+            let git_email_output = Command::new("git")
+                .args(["config", "--global", "user.email", &config.email])
+                .output()
+                .unwrap();
+
+            stdout()
+                .write_all(&git_name_output.stdout)
+                .expect("Failed to print output from git_name_output");
+            stdout()
+                .write_all(&git_email_output.stdout)
+                .expect("Failed to print output from git_email_output");
+        }
+
+        None => {
+            println!("Invalid alias!");
+        }
+    }
 }
